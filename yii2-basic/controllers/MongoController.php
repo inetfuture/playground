4d48721398c59e936f381a2040ade1be5103f5ee
inetfuture(Aaron Wang) <inetfuture@gmail.com>
2015-10-19 11:24:18 +0800

    yii2-basic: init

---
+++ yii2-basic/controllers/MongoController.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class MongoController extends Controller
+{
+    public function actionIndex()
+    {
+        return 'haha';
+    }
+}
+
